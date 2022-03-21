5a3eb9195d7ff88acace96955e10da88cc76ca85
lakshya.inexture <lakshya.inexture@gmail.com>
2022-03-21 11:22:41 +0530

    modifications

--- backend/server.js
+++ backend/server.js
@@ -14,12 +14,14 @@ app.use(cors());
 app.use("/api", require("./routes/userRoute"));
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("build"));
+    app.use(express.static(path.join(__dirname, "../build")));
 
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../build", "index.html"));
+        res.sendFile(path.resolve(__dirname, "../build", "index.html"));
     });
 }
+
+// console.log(path.join(__dirname, "../build"));
 
 const url = process.env.MONGO_URL;
 
